813101c0ae2a695d20bf23177337118b2f788a16
Benny <hoaben13@student.westerdals.no>
2016-09-29 13:30:20 +0200

    setting html objects from javascript

---
+++ Forelesning5/script/google-map-app.js
@@ -0,0 +1,30 @@
+$(function () {
+    //HTML Objects
+    var $getEarthQuakesBtn;
+    var $markerInformationSpan;
+    var $googleMapSection;
+
+    //Google map Objects
+    var googleMapObject = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+
+    //US GOV API URL
+
+    //init
+    var init = function () {
+        var setHTMLObjects = function () {
+            $getEarthQuakesBtn = $("#getEarthQuakesBtn");
+            $markerInformationSpan = $("#markerInformationSpan");
+            $googleMapSection = $("#googleMapSection");
+        } ();
+
+        var setEvents = function () {
+
+        } ();
+
+        var initGoogleMap = function () {
+            
+        } ();
+    } ();
+
+    //application logic
+});
