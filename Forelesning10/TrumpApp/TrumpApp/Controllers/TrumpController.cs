3f58b662523a0a128850f376294baa73f01d5e2c
Benny <hoaben13@student.westerdals.no>
2016-11-10 15:16:04 +0100

    web-api in C#

---
+++ Forelesning10/TrumpApp/TrumpApp/Controllers/TrumpController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Linq;
+
+namespace TrumpApp.Controllers
+{
+    public class TrumpController : ApiController
+    {
+        public HttpRequestMessage AddFact()
+        {
+            
+        }
+        public HttpResponseMessage GetFacts()
+        {
+            XElement factsXML = XElement.Load(GetFilePath());
+
+            var factList = from facts in factsXML.Descendants("funFact")
+                           select facts;
+
+            return Request.CreateResponse(HttpStatusCode.OK, factList);
+        }
+
+        public String GetFilePath()
+        {
+            return System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/trumpFunFacts.xml");
+        }
+    }
+}
---
+++ Forelesning10/TrumpApp/TrumpApp/Models/FunFact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrumpApp.Models
+{
+    class FunFact
+    {
+        public int Id { get; set; }
+        public string fact { get; set; }
+    }
+}
