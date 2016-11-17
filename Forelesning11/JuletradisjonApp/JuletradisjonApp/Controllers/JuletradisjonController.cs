de94ab18131dc5e5c6f7743b45c22e7a2740c3e1
Benny <hoaben13@student.westerdals.no>
2016-11-17 20:47:10 +0100

    finished tutorial from class. Also added POST functionality for extra and code-candy

---
+++ Forelesning11/JuletradisjonApp/JuletradisjonApp/Controllers/JuletradisjonController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using JuletradisjonApp.Models;
+
+namespace JuletradisjonApp.Controllers
+{
+
+    public class JuletradisjonController : ApiController
+    {
+        public HttpResponseMessage PostJuletradisjon(Juletradisjon _juletradisjon)
+        {
+            XElement juletradisjonXML = XElement.Load(GetFilePath());
+
+            juletradisjonXML.Add(
+                new XElement("juletradisjon",
+                    new XElement("id", _juletradisjon.Id),
+                    new XElement("tekst", _juletradisjon.Tekst),
+                    new XElement("opprinnelse", _juletradisjon.Opprinnelse)
+                )
+              );
+            juletradisjonXML.Save(GetFilePath());
+            return Request.CreateResponse(HttpStatusCode.Created, _juletradisjon);
+        }
+        public HttpResponseMessage DeleteJuletradisjon(int id)
+        {
+            XElement juletradisjonXML = XElement.Load(GetFilePath());
+
+            var valgtJuletradisjon = (from juletradisjon in juletradisjonXML.Descendants("juletradisjon")
+                                      where (int)juletradisjon.Element("id") == id
+                                      select juletradisjon).SingleOrDefault();
+            valgtJuletradisjon.Remove();
+
+            juletradisjonXML.Save(GetFilePath());
+            return Request.CreateResponse(HttpStatusCode.OK, valgtJuletradisjon);
+        }
+        public HttpResponseMessage PutJuletradisjon(Juletradisjon _juletradisjon)
+        {
+            XElement juletradisjonXML = XElement.Load(GetFilePath());
+            var valgtJuletradisjon = (from juletradisjon in juletradisjonXML.Descendants("juletradisjon")
+                                      where (int)juletradisjon.Element("id") == _juletradisjon.Id
+                                      select juletradisjon).SingleOrDefault();
+            valgtJuletradisjon.SetElementValue("tekst", _juletradisjon.Tekst);
+            valgtJuletradisjon.SetElementValue("opprinnelse", _juletradisjon.Opprinnelse);
+
+            juletradisjonXML.Save(GetFilePath());
+
+            return Request.CreateResponse(HttpStatusCode.OK, valgtJuletradisjon);
+        }
+        public HttpResponseMessage GetJuletradisjon(int? id)
+        {
+            //Step 1: connecto to DB
+            XElement juletradisjonXML = XElement.Load(GetFilePath());
+            //Step 2: LINQ-query
+            var valgtJuletradisjon = (from juletradisjon in juletradisjonXML.Descendants("juletradisjon")
+                                      where (int)juletradisjon.Element("id") == id
+                                      select juletradisjon).SingleOrDefault();
+            return Request.CreateResponse(HttpStatusCode.OK, valgtJuletradisjon);
+
+        }
+
+        private String GetFilePath()
+        {
+            return System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/juletradisjoner.xml");
+        }
+    }
+}
---
+++ Forelesning11/JuletradisjonApp/JuletradisjonApp/Models/Juletradisjon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuletradisjonApp.Models
+{
+    public class Juletradisjon
+    {
+        public int Id { get; set; }
+        public String Tekst { get; set; }
+        public String Opprinnelse { get; set; }
+    }
+}
