df66726c4baae585c4666caeb5de963c20285e39
Benny <hoaben13@student.westerdals.no>
2016-11-24 13:22:01 +0100

    last forelesning

---
+++ Forelesning12/uploadApp/uploadApp/Controllers/UploadImageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace uploadApp.Controllers
+{
+    public class UploadImageController : ApiController
+    {
+        public HttpResponseMessage UploadImage()
+        {
+            if (System.Web.HttpContext.Current.Request.Files != null)
+            {
+                var file = System.Web.HttpContext.Current.Request.Files[0];
+                String fileName = file.FileName;
+
+                file.SaveAs(GetFilePath(fileName));
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, "OK");
+        }
+
+        private String GetFilePath(String fileName)
+        {
+            return System.Web.Hosting.HostingEnvironment.MapPath(@"~/Images/" + fileName);
+        }
+    }
+}
